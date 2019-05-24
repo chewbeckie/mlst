ea2cc093a3d05fb4b7b39a1f93f7010dcebc83d5
Dan Fornika <dfornika@gmail.com>
2019-05-24 09:47:50 -0700

    Restrucure code and download to pubmlst dir

--- scripts/pmlst-download_pub_mlst.pl
+++ scripts/pmlst-download_pub_mlst.pl
@@ -4,10 +4,17 @@ use strict;
 use HTTP::Tiny;
 use JSON;
 use Data::Dumper;
+
+#..............................................................................
+# Globals
+
 use constant BASE_URI => 'http://rest.pubmlst.org';
+my $OUTDIR = 'pubmlst';
 
 my $HTTP = HTTP::Tiny->new();
 my $schemes_uri = BASE_URI . '/db/pubmlst_plasmid_isolates/schemes';
+
+#..............................................................................
 
 sub get_json {
     my ($uri) = @_;
@@ -15,12 +22,6 @@ sub get_json {
     my $response_content_json = decode_json $response->{content};
     return $response_content_json;
 }
-
-my @schemes = @{ get_json($schemes_uri)->{schemes} };
-my $num_schemes = @schemes;
-
-print STDERR "There are $num_schemes pMLST schemes.\n";
-
 
 sub process_mlst_scheme_name {
     my ($scheme_name) = @_;
@@ -33,16 +34,26 @@ sub download_pmlst_scheme_alleles {
     my ($scheme_name, $scheme_uri, $scheme_dir) = @_;
     (my $seqdef_uri = $scheme_uri) =~ s/isolates/seqdef/; 
     my @loci = @{ get_json($seqdef_uri)->{loci} };
-    system "mkdir $scheme_dir";
+    my $scheme_dir = $OUTDIR . '/' . $scheme_dir;
+    my $cmd = "mkdir -p $scheme_dir";
+    qx($cmd);
     foreach my $locus (@loci) {
 	print $locus . "\n";
 	my $locus_info = get_json($locus);
 	my $locus_id = $locus_info->{id};
 	my $locus_fasta_uri = $locus_info->{alleles_fasta};
 	my $locus_filename = $locus_id . ".tfa";
-	system "cd $scheme_dir && wget -q -O $locus_filename $locus_fasta_uri";
+	$cmd = "cd \Q$scheme_dir\E && wget -q -O \Q$locus_filename\E \Q$locus_fasta_uri\E";
+	qx($cmd);
     }
 }
+
+#..............................................................................
+
+my @schemes = @{ get_json($schemes_uri)->{schemes} };
+my $num_schemes = @schemes;
+
+print STDERR "There are $num_schemes pMLST schemes.\n";
 
 foreach my $scheme (@schemes) {
     my $scheme_uri = $scheme->{scheme};
@@ -54,3 +65,4 @@ foreach my $scheme (@schemes) {
 
 }
 
+#..............................................................................
